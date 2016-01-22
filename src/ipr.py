22e3f183a0deda5b211310ee8b0652f444c1e421
Jacques Dainat <jacques.dainat@bils.se>
2016-01-22 16:24:54 +0100

    fix retrieving of pathway information (last columns of the ipr output)

--- src/ipr.py
+++ src/ipr.py
@@ -12,8 +12,8 @@ def read_ipr(io_buffer, whitelist=None):
         columns = line.split("\t") #columns are assumed to be tab-separated
         #if column exists and dbxref is in whitelist (aside from whitespace padding and caps)
         if (len(columns)>3 and (columns[3].strip().lower() in whitelist)) or\
-		 (len(columns)>3 and not whitelist): 
-	    ipr_list.append(Annotation(columns[0].strip(), "Dbxref", columns[3].strip().upper()+":"+columns[4].strip()))
+		 (len(columns)>3 and not whitelist):
+            ipr_list.append(Annotation(columns[0].strip(), "Dbxref", columns[3].strip().upper()+":"+columns[4].strip()))
         #if column exists (we don't care about the whitelist for GO annotations)
         if len(columns)>13 and columns[13].find("GO:") != -1: 
             ipr_list.append(Annotation(columns[0].strip(), "Dbxref", columns[13].strip()))
